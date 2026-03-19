46dc4c5f0371060a58c5f1b1936c6668ee9c467a
cole-johnson-ctk <ctjohn2@ilstu.edu>
2026-03-19 01:50:18 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/valley-bloom-showcase/" : "/",
+  base: mode === "production" ? "/valleybloomshowcase/" : "/",
   server: {
     host: "::",
     port: 8080,
